f89cfabdef8aa1d86a474d24a81d2a80f1542483
baltasar <baltasarq@gmail.com>
2019-08-22 22:25:55 +0200

    Added calendar support

--- model/appinfo.py
+++ model/appinfo.py
@@ -5,7 +5,7 @@
 
 class AppInfo:
     Name = "cnescualos"
-    CalendarURL = "http://calendar.google.com"
+    CalendarURL = "http://calendar.google.com/calendar?cid=dDRyMGo2N3VtYWpmcmN1Y29hbDN1NzF2OXNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
     Title = "C.N. Escualos"
     Version = "20160008-0101"
     Author = "contact@baltasarq.info"
