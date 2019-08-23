456f8b15a1c05a999a5f0c68f8fbf44dd8b64fe5
baltasar <baltasarq@gmail.com>
2019-08-23 11:58:23 +0200

    Added support for records shown divided in styles and distance

--- handlers/member/record/show_all.py
+++ handlers/member/record/show_all.py
@@ -13,15 +13,15 @@ from model.record_entry import SwimStyles
 
 
 class Record:
-    def __init__(self, dni, surname, name, record_entry):
-        self.added = record_entry.added
+    def __init__(self, added, dni, surname, name, distance, style, millis, where):
+        self.added = added
         self.dni = dni
         self.name = name
         self.surname = surname
-        self.distance = record_entry.distance
-        self.style = SwimStyles.name_from_id(record_entry.style_id)
-        self.millis = record_entry.milliseconds
-        self.where = record_entry.where
+        self.distance = distance
+        self.style = style
+        self.millis = millis
+        self.where = where
 
     def sorting_key(self):
         return (self.distance * 10) + self.millis
@@ -39,43 +39,66 @@ class Record:
 class ShowAllRecordsHandler(webapp2.RequestHandler):
     def get(self):
         try:
-            records_per_distance = {}
+            records_per_distance_and_style = {}
             members = Member.query()
 
             # Get current user, but anyway this section is public
             usr = Member.retrieve_usr_data()
 
             # Collect all data
+            # Create a dictionary distance -> style -> records_list
             for m in members:
                 for record_entry in m.records:
+                    style = SwimStyles.name_from_id(record_entry.style_id)
                     distance = record_entry.distance
-                    records_list = records_per_distance.get(distance)
+                    records_dict = records_per_distance_and_style.get(distance)
 
+                    # Retrieve distance dictionary for all styles in that distance
+                    if not records_dict:
+                        records_dict = {}
+                        records_per_distance_and_style[distance] = records_dict
+
+                    # Retrieve records list for that style
+                    records_list = records_dict.get(style)
                     if not records_list:
                         records_list = []
-                        records_per_distance[distance] = records_list
+                        records_dict[style] = records_list
 
-                    records_list.append(Record(
-                        m.dni,
-                        m.surname,
-                        m.name,
-                        record_entry))
+                    records_list.append(
+                        Record(
+                            record_entry.added,
+                            m.dni,
+                            m.surname,
+                            m.name,
+                            distance,
+                            style,
+                            record_entry.milliseconds,
+                            record_entry.where))
 
             # Sort data
-            distances = records_per_distance.keys()
+            distances = list(records_per_distance_and_style.keys())
             distances.sort()
 
-            for pair in records_per_distance.items():
-                pair[1].sort(key=lambda r: r.sorting_key())
+            for pair_distance_styles in records_per_distance_and_style.items():
+                for style in pair_distance_styles[1].keys():
+                    pair_distance_styles[1][style].sort(key=lambda r: r.sorting_key())
 
+            # Prepare styles info
+            styles = []
+            for i in range(SwimStyles.count()):
+                styles.append(SwimStyles.name_from_id(i))
+
+            styles.sort()
+
+            # Render answer
             template_values = {
                 "info": AppInfo,
                 "usr": usr,
                 "distances": distances,
-                "records_per_distance": records_per_distance,
+                "styles": styles,
+                "records_per_distance_and_style": records_per_distance_and_style,
             }
 
-            # Render answer
             jinja = jinja2.get_jinja2(app=self.app)
             self.response.write(jinja.render_template("records.html", **template_values))
         except Exception as e:
